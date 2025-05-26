139e6df7a5265b0e7c5ff547ac68a37d68654e58
hu-junsheng <164840895+hu-junsheng@users.noreply.github.com>
2025-05-26 11:26:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,14 +13,10 @@ def args_parse():
     parser.add_argument('--train', action="store_true")
     parser.add_argument('--test', action="store_true")
     parser.add_argument('--resume', action="store_true")
-    parser.add_argument('--plot_iter', type=int, default=500)
-    parser.add_argument('--plot_train', action="store_true", help='plot train samples')
-    parser.add_argument('--plot_test', action="store_true", help='plot test samples')
-    parser.add_argument('--board_iter', type=int, default=500)
     parser.add_argument('--save_iter', type=int, default=2000)
     parser.add_argument('--load_epoch', type=int, default=0)
     parser.add_argument('--load_step', type=int, default=0)
-    parser.add_argument('--total_epochs', type=int, default=100)
+    parser.add_argument('--total_epochs', type=int, default=300)
     parser.add_argument('--valid_epoch', type=int, default=1)
     parser.add_argument('--ckpt', type=str, default=None, help='load checkpoint')
     parser.add_argument('--test_dir', type=str, default=None, help='directory path of ImageNet dataset')
@@ -31,25 +27,14 @@ def args_parse():
     parser.add_argument('--test_fold', type=int, default=5, help='k for test')
     parser.add_argument('--model', type=str, default='hyperfilm')
     parser.add_argument('--exp_name', type=str, default=None)
-    # ------------------------------
-    # DDP
-    # ------------------------------
+
     parser.add_argument('--gpu_id', type=int, default=5,help='ID of the GPU to use (default: 0)')
-    parser.add_argument('--n_nodes', default=1, type=int)
-    parser.add_argument('--rank', default=0, type=int, help='ranking within the nodes')
-    parser.add_argument('--workers', default=4, type=int)
     parser.add_argument('--num_workers', type=int, default=8)
-    parser.add_argument('--port', default='1235', type=str, help='port')
     # ------------------------------
     # Network
     # ------------------------------
-    parser.add_argument('--cnn_layers', type=int, default=5)
-    parser.add_argument('--rnn_layers', type=int, default=0)
     parser.add_argument('--in_ch', type=int, default=16)
     parser.add_argument('--out_ch', type=int, default=1)
-    parser.add_argument('--condition', type=str, default='hyper')
-    parser.add_argument('--film_dim', type=str, default='chan')
-    parser.add_argument('--without_anm', action='store_true')
     # ------------------------------
     # Data
     # ------------------------------
@@ -83,12 +68,7 @@ def args_parse():
     parser.add_argument("--limit_lr", default=9e-5, type=float)
     parser.add_argument("--train_num", default=19, type=int)
     parser.add_argument("--seeds", default=0, type=int)
-
-
-
-
-
-
+    
     args = parser.parse_args()
     return args
 
